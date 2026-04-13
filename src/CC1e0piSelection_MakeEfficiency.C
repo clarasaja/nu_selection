574de2dea55175a2c2b2a793262d7178530f95ff
csaia <clara.saia.1997@gmail.com>
2026-04-13 07:43:29 -0500

    Add MakeEfficiency Purity and Plots

---
+++ src/CC1e0piSelection_MakeEfficiency.C
@@ -0,0 +1,109 @@
+#include "sbnana/CAFAna/Core/SpectrumLoader.h"
+#include "sbnana/CAFAna/Core/Spectrum.h"
+
+// helpers
+#include "./helpers/CC1e0piSelection_Cuts.h"
+#include "./helpers/CC1e0piSelection_TruthCuts.h"
+#include "./helpers/CC1e0piSelection_Efficiency.h"
+
+
+// ROOT stuff
+#include "TCanvas.h"
+#include "TFile.h"
+#include "TTreeReader.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TLegend.h"
+#include "TPaveText.h"
+#include "TStyle.h"
+#include "THStack.h"
+#include "TEfficiency.h"
+#include "TGraphAsymmErrors.h"
+#include "TLine.h"
+#include "TROOT.h"
+
+using namespace ana;
+
+void CC1e0piSelection_MakeEfficiency() {
+
+    // CNAF NuMI MC
+    const std::string TargetFile = " "; 
+    
+
+    SpectrumLoader NuLoader(TargetFile);
+
+    // selection efficiency
+    std::vector<double> TrueEnergyBinning = {0., 0.2, 0.4, 0.6, 0.8, 1, 1.2, 1.4, 1.6, 1.8, 2., 2.4, 2.8, 3.2, 4};
+
+    Spectrum* sTrueNeutrinoEnergy = new Spectrum("E_{#nu} [GeV]", 
+                                                 Binning::Custom(TrueEnergyBinning),
+                                                 NuLoader,
+                                                 kCC1e0p1Signal_TrueNeutrinoEnergy, 
+                                                 kNoSpillCut);
+
+    const unsigned int kNSelectionSteps = SelectionSteps.size();
+    Spectrum *sTrueNeutrinoEnergy_SelectionSteps[kNSelectionSteps];
+
+    for(unsigned int iSel = 0; iSel < kNSelectionSteps; ++iSel) {
+        sTrueNeutrinoEnergy_SelectionSteps[iSel] = new Spectrum("E_{#nu} [GeV]", 
+                                                                Binning::Custom(TrueEnergyBinning),
+                                                                NuLoader, 
+                                                                kCC1e0p1Signal_TrueNeutrinoEnergy_MakeSelectionStep(SelectionSteps[iSel].cut),
+                                                                kNoSpillCut);//kCRTPMTNeutrino); 
+    }
+
+    NuLoader.Go();
+    
+    gROOT->SetBatch(kTRUE);
+    TFile FOut("./root/CC1e0piSelection_Efficiency.root", "recreate");
+    double TargetPOT;
+
+    // selection efficiency
+    TCanvas* cEffProg = new TCanvas("efficiencyCC1e0piselection", "efficiencyCC1e0piselection", 500, 500);
+    TLegend* lEffProg = new TLegend(0.125, 0.7, 0.9, 0.875, "NuMI CV");
+    lEffProg->SetNColumns(3);
+
+    TargetPOT = sTrueNeutrinoEnergy->POT();                   
+    TH1* hTrue = sTrueNeutrinoEnergy->ToTH1(TargetPOT);
+    hTrue->SetFillColorAlpha(kGray, 0.5);
+    hTrue->SetLineColor(0); 
+    hTrue->SetTitle(";E_{#nu} [GeV];Selection efficiency");
+    TH1* hTrue_Scaled = (TH1*) hTrue->Clone();
+    hTrue_Scaled->Scale(1. / hTrue->GetMaximum());
+    hTrue_Scaled->GetYaxis()->SetRangeUser(0, 1.4);
+    hTrue_Scaled->Draw("HIST SAME");
+
+    TLine *line = new TLine(0, 1, 4, 1);
+    line->Draw("SAME");
+
+    for(unsigned int iSel = 0; iSel < kNSelectionSteps; ++iSel) {
+        TargetPOT = sTrueNeutrinoEnergy_SelectionSteps[iSel]->POT();
+        TH1* h = sTrueNeutrinoEnergy_SelectionSteps[iSel]->ToTH1(TargetPOT);
+        h->SetLineColor(SelectionSteps[iSel].color);
+
+        TEfficiency* eff = new TEfficiency(*h, *hTrue);
+        eff->SetLineWidth(2);
+        eff->SetLineColor(SelectionSteps[iSel].color);
+        eff->SetMarkerColor(SelectionSteps[iSel].color);
+
+        std::cout << h->GetEntries() << "\t" << hTrue->GetEntries() << "\t" << h->GetEntries() / hTrue->GetEntries() << std::endl;
+        TString labelEffProg = SelectionSteps[iSel].label + Form(" (%.0f%%)", 100.* h->GetEntries() / hTrue->GetEntries());
+        lEffProg->AddEntry(h, labelEffProg, "f");
+
+        eff->Draw("P SAME");
+        gPad->Update();
+    }
+
+    lEffProg->SetTextSize(0.0275);
+    lEffProg->SetFillStyle(0);
+    lEffProg->Draw();
+    cEffProg->Write();
+
+    gStyle->SetLineScalePS(5);
+    cEffProg->SaveAs("plots/CC1e0piSelection_Efficiency.pdf");
+    delete cEffProg;
+
+    FOut.Close();
+
+    return;
+}
---
+++ src/CC1e0piSelection_MakePurity.C
@@ -0,0 +1,147 @@
+#include "sbnana/CAFAna/Core/SpectrumLoader.h"
+#include "sbnana/CAFAna/Core/Spectrum.h"
+
+// helpers
+#include "./helpers/CC1e0piSelection_Cuts.h"
+#include "./helpers/CC1e0piSelection_TruthCuts.h"
+
+// ROOT stuff
+#include "TCanvas.h"
+#include "TFile.h"
+#include "TTreeReader.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TLegend.h"
+#include "TPaveText.h"
+#include "TStyle.h"
+#include "THStack.h"
+#include "TEfficiency.h"
+#include "TGraphAsymmErrors.h"
+#include "TLine.h"
+
+using namespace ana;
+
+void CC1e0piSelection_MakePurity() {
+    
+    // CNAF NuMI MC
+    // const std::string TargetFile = "/storage/gpfs_data/icarus/local/users/cfarnese/NUMI/NUMI_MC/*.root"; ///< CV
+    // const std::string TargetFile = "/storage/gpfs_data/icarus/plain/user/cfarnese/RT_production_NuMIreference_20May25/mc*/caf_here/*.flat.caf.root"; ///< reference
+    // const std::string TargetFile = "/storage/gpfs_data/icarus/plain/user/cfarnese/RT_NUMI_nuonly_May18/run*/cafmakerjob_here_2d_updated/*.flat.caf.root"; ///< new BDT
+
+    // FNAL NuMI MC
+    // const std::string TargetFile = "/exp/icarus/data/users/rtriozzi/mc/numi_FRFIX/concat_NuMI_MC_FRFIX_*.root";
+
+    // FNAL development NuMI MC
+    // const std::string TargetFile = "/pnfs/icarus/persistent/users/rtriozzi/nugraph/standard/numinom.flat.caf.root"; ///< NuE
+    //const std::string TargetFile = "/pnfs/icarus/persistent/users/rtriozzi/nugraph/standard/numinom.flat.caf.root"; ///< nominal flux, mostly NuMu
+
+    // FNAL development NuMI MC / NG2 filter + NG2 PID
+    const std::string TargetFile = "/pnfs/icarus/persistent/users/rtriozzi/nugraph/nugraphreco/numinue_NuGraphReco.flat.caf.root";
+
+    SpectrumLoader NuLoader(TargetFile);
+
+    // selection purity at each selection step
+    const unsigned int kNVar = SelectionPlots.size();
+    const unsigned int kNSel = SelectionSteps_NoTrigger.size();
+    Spectrum *spectra_Signal[kNVar];
+    Spectrum *spectra_SelectedAll[kNVar][kNSel];
+    Spectrum *spectra_SelectedSignal[kNVar][kNSel];
+
+    for(unsigned int iVar = 0; iVar < kNVar; ++iVar) {
+        spectra_Signal[iVar] = new Spectrum(SelectionPlots[iVar].label, 
+                                            SelectionPlots[iVar].bins, 
+                                            NuLoader, 
+                                            SelectionPlots[iVar].var, 
+                                            kNoSpillCut,
+                                            kTrueCC1e0pi);  
+
+        for(unsigned int jSel = 0; jSel < kNSel; ++jSel) {
+            spectra_SelectedAll[iVar][jSel] = new Spectrum(SelectionPlots[iVar].label, 
+                                                           SelectionPlots[iVar].bins, 
+                                                           NuLoader, 
+                                                           SelectionPlots[iVar].var, 
+                                                           kNoSpillCut, // kCRTPMTNeutrino,
+                                                           SelectionSteps_NoTrigger[jSel].cut);     
+
+            spectra_SelectedSignal[iVar][jSel] = new Spectrum(SelectionPlots[iVar].label, 
+                                                              SelectionPlots[iVar].bins, 
+                                                              NuLoader, 
+                                                              SelectionPlots[iVar].var, 
+                                                              kNoSpillCut, // kCRTPMTNeutrino,
+                                                              SelectionSteps_NoTrigger[jSel].cut && kTrueCC1e0pi);    
+        }
+    }
+
+    NuLoader.Go();
+ 
+    TFile FOut("./root/CC1e0piSelection_Purity.root", "recreate");
+
+    TCanvas *c[kNVar];
+    TLegend *l[kNVar];
+    double TargetPOT;
+    std::string title;
+
+    for(unsigned int iVar = 0; iVar < kNVar; ++iVar) {
+
+        c[iVar] = new TCanvas(SelectionPlots[iVar].suffix.c_str(), SelectionPlots[iVar].suffix.c_str(), 500, 500);
+        l[iVar] = new TLegend(0.125, 0.7, 0.9, 0.875, "NuMI CV");
+        l[iVar]->SetNColumns(3);
+
+        TargetPOT = spectra_Signal[iVar]->POT();
+        TH1* hSignal = spectra_Signal[iVar]->ToTH1(TargetPOT);
+        hSignal->SetFillColorAlpha(kGray, 0.5);
+        hSignal->SetLineColor(0); 
+        title = std::string(";") + SelectionPlots[iVar].label + std::string(";Selection purity");
+        hSignal->SetTitle(title.c_str());
+        TH1* hSignal_Scaled = (TH1*) hSignal->Clone();
+        hSignal_Scaled->Scale(1. / hSignal->GetMaximum());
+        hSignal_Scaled->GetYaxis()->SetRangeUser(0, 1.4);
+        hSignal_Scaled->Draw("HIST SAME");
+
+        TLine *line = new TLine(0, 1, 4, 1);
+        line->Draw("SAME");
+
+        for(unsigned int jSel = 0; jSel < kNSel; ++jSel) {
+            // all the selected reconstructed interactions
+            TargetPOT = spectra_SelectedAll[iVar][jSel]->POT();
+            TH1* hAll = spectra_SelectedAll[iVar][jSel]->ToTH1(TargetPOT);
+
+            // selected reconstructed interactions matching signal
+            TargetPOT = spectra_SelectedSignal[iVar][jSel]->POT();
+            TH1* hSelected = spectra_SelectedSignal[iVar][jSel]->ToTH1(TargetPOT);
+
+            TString labelEffProg = SelectionSteps_NoTrigger[jSel].label + Form(" (%.0f%%)", 100.* hSelected->GetEntries() / hAll->GetEntries());
+
+            title = std::string(";") + SelectionPlots[iVar].label + std::string(";Purity");
+            TEfficiency* eff = new TEfficiency("eff", title.c_str(), SelectionPlots[iVar].bins.NBins(), &SelectionPlots[iVar].bins.Edges()[0]);
+
+            for (int i = 1; i <= SelectionPlots[iVar].bins.NBins(); i++) {
+                if (hAll->GetBinContent(i) == 0) continue;
+                eff->SetTotalEvents(i, hAll->GetBinContent(i));  
+                eff->SetPassedEvents(i, hSelected->GetBinContent(i));
+            }
+
+            eff->SetLineWidth(2);
+            eff->SetLineColor(SelectionSteps_NoTrigger[jSel].color);
+            eff->SetMarkerColor(SelectionSteps_NoTrigger[jSel].color);
+
+            l[iVar]->AddEntry(eff, labelEffProg, "f");
+
+            eff->Draw("P SAME");
+            gPad->Update();
+        }        
+
+        l[iVar]->SetFillStyle(0);
+        l[iVar]->SetTextSize(0.0275);
+        l[iVar]->Draw();
+        c[iVar]->Write();
+        title = std::string("plots/") + SelectionPlots[iVar].suffix + std::string("_Purity.pdf");
+
+        gStyle->SetLineScalePS(5);
+        c[iVar]->SaveAs(title.c_str());
+    }
+
+    FOut.Close();
+
+    return;
+}
